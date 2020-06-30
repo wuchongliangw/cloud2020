20591265f1a4458ef165bebc3e10c39809ba1aee
wuchongliang <13640224674@139.com>
2020-06-30 18:05:25 +0800

    commit

---
+++ cloud-config-client-3366/src/main/java/com/meamei/ConfigClientMain3366.java
@@ -0,0 +1,19 @@
+package com.meamei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author mm013
+ * @create 2020-06-30 15:40:25
+ * @description:
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3366 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class, args);
+    }
+}
---
+++ cloud-config-client-3366/src/main/java/com/meamei/controller/ConfigClientController.java
@@ -0,0 +1,27 @@
+package com.meamei.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author mm013
+ * @create 2020-06-30 14:03:37
+ * @description:
+ */
+@RestController
+@RefreshScope
+public class ConfigClientController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo() {
+        return "serverPort: " + serverPort + "\t\n\n configInfo: "  + configInfo;
+    }
+}
